5156874936eac8cc547a643ee932d33dc25d334c
RHRakib <85560900+RHRakibhasan@users.noreply.github.com>
2022-07-20 17:53:00 +0600

    sourse code upload
    Its a HTML CSS task

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+    "use strict";
+
+// niceSelect
+$('select').niceSelect(); 
+
+ // mean menu active 
+ jQuery('#mobile').meanmenu({
+    meanMenuContainer: '.mobile-menu',
+    meanScreenWidth: "991"
+});
+
+ 
+
+
+
+
+
+
+})(jQuery);	
